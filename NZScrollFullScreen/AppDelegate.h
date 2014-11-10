9dc38419041d7f6e074613b79c45040460adc4c5
Anonymous <nazrinalmi@YHack-MPro.local>
2014-11-10 15:51:50 +0800

    init file

---
+++ NZScrollFullScreen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NZScrollFullScreen
+//
+//  Created by Anonymous on 11/10/14.
+//  Copyright (c) 2014 Nazrin Almi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NZScrollFullScreen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NZScrollFullScreen
+//
+//  Created by Anonymous on 11/10/14.
+//  Copyright (c) 2014 Nazrin Almi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
